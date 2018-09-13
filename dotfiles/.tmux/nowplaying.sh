915f514502f3c93cde34a84bb7bf49572e653878
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-09-13 13:01:54 -0600

    Tmux: Update nowplaying to long titles
    * Also refactored it to unify behavior between spotify and itunes
        * Either set an intermediate value and they combine later

--- dotfiles/.tmux/nowplaying.sh
+++ dotfiles/.tmux/nowplaying.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
-command -v osascript > /dev/null 2>&1  && osascript -e 'set track_str to ""
+command -v osascript > /dev/null 2>&1  && osascript -e '
+set track_name to ""
+set artist_name to ""
+set now_playing to false
 if application "iTunes" is running then
-	tell application "iTunes" to if player state is playing then set track_str to "♫ " & name of current track & " ♪ " & artist of current track & " ♫"
+    tell application "iTunes" to if player state is playing then
+        set track_name to name of current track
+        set artist_name to artist of current track
+        set now_playing to true
+    end if
 end if
 
 if application "Spotify" is running then
-	tell application "Spotify" to if player state is playing then set track_str to "♫ " & name of current track & " ♪ " & artist of current track & " ♫"
+    tell application "Spotify" to if player state is playing then
+        set track_name to name of current track
+        set artist_name to artist of current track
+        set now_playing to true
+    end if
 end if
 
-track_str'
+if now_playing then
+    if length of track_name > 24 then
+        set track_name to (text 1 thru 24 of track_name) & "…"
+    end if
+
+    if length of artist_name > 24 then
+        set artist_name to (text 1 thru 24 of artist_name) & "…"
+    end if
+
+    "♫ " & track_name & " ♪ " & artist_name & " ♫"
+end if
+'
