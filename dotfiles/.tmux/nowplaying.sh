3899f29a9dcb8316a2feb166c59c9bf1ea279f56
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-10-03 13:25:03 -0600

    Tmux: Update nowplaying to truncate better

--- dotfiles/.tmux/nowplaying.sh
+++ dotfiles/.tmux/nowplaying.sh
@@ -4,30 +4,29 @@ set track_name to ""
 set artist_name to ""
 set now_playing to false
 if application "iTunes" is running then
-    tell application "iTunes" to if player state is playing then
-        set track_name to name of current track
-        set artist_name to artist of current track
-        set now_playing to true
-    end if
+  tell application "iTunes" to if player state is playing then
+    set track_name to name of current track
+    set artist_name to artist of current track
+    set now_playing to true
+  end if
 end if
 
 if application "Spotify" is running then
-    tell application "Spotify" to if player state is playing then
-        set track_name to name of current track
-        set artist_name to artist of current track
-        set now_playing to true
-    end if
+  tell application "Spotify" to if player state is playing then
+    set track_name to name of current track
+    set artist_name to artist of current track
+    set now_playing to true
+  end if
 end if
 
 if now_playing then
-    if length of track_name > 24 then
-        set track_name to (text 1 thru 24 of track_name) & "…"
+  repeat while length of ("♫ " & track_name & " ♪ " & artist_name & " ♫") > 50
+    if length of track_name > length of artist_name then
+      set track_name to (text 1 thru -3 of track_name) & "…"
+    else
+      set artist_name to (text 1 thru -3 of artist_name) & "…"
     end if
-
-    if length of artist_name > 24 then
-        set artist_name to (text 1 thru 24 of artist_name) & "…"
-    end if
-
-    "♫ " & track_name & " ♪ " & artist_name & " ♫"
+  end repeat
+  "♫ " & track_name & " ♪ " & artist_name & " ♫"
 end if
 '
