47d902e0c4f779731e0efaf0439cc51cb563fde6
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-10-24 18:55:06 -0600

    Tmux: Centralize powerline glyphs for status
    * Now one file controls both statuses

---
+++ dotfiles/.tmux/glyphs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Powerline font glyphs
+# This file contains the actual powerline symbols
+# You'll need a powerline font to see them properly
+POWERLINE_ENABLE=true
+export PL_RIGHT_BLACK=$'\ue0b0'
+export PL_RIGHT=$'\ue0b1'
+export PL_LEFT_BLACK=$'\ue0b2'
+export PL_LEFT=$'\ue0b3'
+
+if [[ $POWERLINE_ENABLE = false ]]; then
+    PL_RIGHT_BLACK=$'\u2551'
+    PL_RIGHT=$'\u2502'
+    PL_LEFT_BLACK=$'\u2551'
+    PL_LEFT=$'\u2502'
+fi
