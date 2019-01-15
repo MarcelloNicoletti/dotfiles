3d0b6e528b1c6f976d7d1668bf2bd030b0026e53
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2019-01-15 10:19:59 -0700

    Tmux: Add ability to count up to countdown.sh

--- dotfiles/.tmux/countdown.sh
+++ dotfiles/.tmux/countdown.sh
@@ -14,31 +14,38 @@ function displaytime {
     # EG: 30 days to ... vs 30 days 12 hours 3 minutes to ...
     # but then 7 days 12 hours to ... and so on
     local T="$1"
+    local sep="to"
+
+    if [[ $T -lt 0 ]] && [[ $3 ]]; then
+        sep="since"
+        T=$((T * -1))
+    fi
+
     local D="$((T/60/60/24))"
     local H="$((T/60/60%24))"
     local M="$((T/60%60))"
     local S="$((T%60))"
     if [ "$D" -gt 7 ]; then
-        printf "%dd to %s\\n" "$D" "$2"
+        printf "%dd %s %s\\n" "$D" "$sep" "$2"
     elif [ "$D" -gt 0 ]; then
         if [ "$H" -gt 0 ]; then
-            printf "%dd %02dh to %s\\n" "$D" "$H" "$2"
+            printf "%dd %02dh %s %s\\n" "$D" "$H" "$sep" "$2"
         else
-            printf "%dd %02dm to %s\\n" "$D" "$M" "$2"
+            printf "%dd %02dm %s %s\\n" "$D" "$M" "$sep" "$2"
         fi
     elif [ "$H" -gt 0 ]; then
         if [ "$M" -gt 0 ]; then
-            printf "%02dh %02dm to %s\\n" "$H" "$M" "$2"
+            printf "%02dh %02dm %s %s\\n" "$H" "$M" "$sep" "$2"
         else
-            printf "%02dh %02ds to %s\\n" "$H" "$S" "$2"
+            printf "%02dh %02ds %s %s\\n" "$H" "$S" "$sep" "$2"
         fi
     elif [ "$M" -gt 0 ]; then
-        printf "%02dm %02ds to %s\\n" "$M" "$S" "$2"
+        printf "%02dm %02ds %s %s\\n" "$M" "$S" "$sep" "$2"
     elif [ "$S" -gt 0 ]; then
-        printf "%02ds to %s\\n" "$S" "$2"
+        printf "%02ds %s %s\\n" "$S" "$sep" "$2"
     else
         printf "%s is done!\\n" "$2"
     fi
 }
 
-displaytime "$duration" "$2"
+displaytime "$duration" "$2" "$3"
